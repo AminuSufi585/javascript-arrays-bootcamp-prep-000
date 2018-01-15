ed203de1d46ad4db3f34f6daa026e6b0aa8f5bc8
Aminu Ibrahim Abubakar <aminukano585@gmail.com>
2018-01-15 06:25:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(arr) {
 function removeElementFromBeginningOfArray(arr) {
    return arr.slice(1);
 }
+
+function destructivelyRemoveElementFromEndOfArray(arr) {
+   arr.pop();
+   return arr;
+}
